8d69116c85775e38a436972c5f334d119d4087a4
Josh <joshua.esterhuizen27@yahoo.com>
2021-10-06 20:27:04 +0200

    Josh_Level Throw section added

---
+++ Assets/Scripts/DevLevelSecret.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DevLevelSecret : MonoBehaviour
+{
+    [SerializeField] public GameObject teleporter; // trigger obj
+
+    void toDevLevel()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    void OnTriggerEnter(Collider other) //When any object collides with the object this script is placed on
+    {
+        if (other.tag == teleporter.tag) // Ensures only the player can trigger the teleportation
+        {
+            toDevLevel();
+        }
+    }
+}
